5e62f9ebcf7aaacd1c0deff8b92f2d7c48ec661d
leompf <lmfraqueiro@gmail.com>
2025-09-15 23:17:05 +0100

    Init web project

---
+++ ContratosCet95.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ContratosCet95.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
